6287c3fe3f9a229879fe8e6e476b52e82ce088ad
cj <cj6209577@sina.com>
2020-07-01 15:37:33 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,6 @@ git commit -m 'deploy'
 
 
 
-# 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
